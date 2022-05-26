9fcf1f773a444abd798c8ec6a1f980de50833354
Natalia Iukhimenko <natalia.yukhimenko@gmail.com>
2022-05-26 13:02:48 +0300

    Added e2e test for register&login scenario

---
+++ cypress/tests/api/registration_tests.js
@@ -0,0 +1,23 @@
+const { faker } = require("@faker-js/faker");
+import {checkStatus200} from "../../support/api/check_utils";
+
+describe("E2E tests", () => {
+    it("Registration & login scenario", () => {
+        const userId = 1;
+        const passwordLength = 6;
+        cy.getSingleUser(userId).then((userResponse => {
+            let newUser = {
+                "email": userResponse.body.data.email,
+                "password": faker.internet.password(passwordLength)
+            }
+            cy.register(newUser).then((registerResponse) => {
+                checkStatus200(userResponse);
+                expect(registerResponse.body.id).to.equal(userId);
+                cy.login(newUser).then((loginResponse) => {
+                    checkStatus200(userResponse);
+                    expect(loginResponse.body.token).to.equal(registerResponse.body.token);
+                })
+            })
+        }));
+    })
+})
